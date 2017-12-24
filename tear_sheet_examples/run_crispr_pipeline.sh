ad438ff68366c15cb617518c6c2e8e9f91d6a7d1
Aaron McKenna <aaronatwpi@gmail.com>
2017-12-23 20:38:09 -0500

    scripts update

--- tear_sheet_examples/run_crispr_pipeline.sh
+++ tear_sheet_examples/run_crispr_pipeline.sh
@@ -2,7 +2,7 @@
 
 java -Xmx4g -jar /app/queue.jar \
  -S /app/sc_GESTALT/pipelines/CRISPR_analysis_PE_V2.scala  \
- -i /app/tear_sheet_examples/no_barcode_splitting_example_tearsheet.txt \
+ -i /app/sc_GESTALT/tear_sheet_examples/no_barcode_splitting_example_tearsheet.txt \
  --aggLocation /var/www/html/testdata/ \
  --expName testdata \
  --eda /app/EDNAFULL.Ns_are_zero \
