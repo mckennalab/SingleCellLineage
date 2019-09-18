9238197a17e03ca5cdb24f4219ff1ec10b08ef37
aaronmck <aaronatwpi@gmail.com>
2019-09-18 15:01:02 -0400

    update the run script for the 10X example

--- tear_sheet_examples/10X_example/run_crispr_pipeline.sh
+++ tear_sheet_examples/10X_example/run_crispr_pipeline.sh
@@ -1,6 +1,6 @@
 java -Xmx4g -jar /app/queue.jar \
  -S /app/sc_GESTALT/pipelines/CRISPR_analysis_PE_V2.scala  \
- -i /app/sc_GESTALT/tear_sheet_examples/basic_example.txt \
+ -i /app/sc_GESTALT/tear_sheet_examples/10X_example/basic_example.txt \
  --aggLocation /var/www/html/test10Xdata/ \
  --expName test10Xdata \
  --eda /app/EDNAFULL.Ns_are_zero \
@@ -10,7 +10,7 @@ java -Xmx4g -jar /app/queue.jar \
  -b /app/bin/ \
  --noTree \
  --scala "/usr/bin/scala -nocompdaemon" \
- --umiLength 16 \
+ --umiLength 26 \
  --primersToUse FORWARD \
  --minimumUMIReads 2 \
  --umiIndex index2
