d5cf2d292e1ed8a440c80608fa94b01db96c5993
aaronmck <aaronatwpi@gmail.com>
2020-08-25 17:15:06 -0400

    lots of cleanup

--- TreeUtils/src/main/scala/annotation/CladeEntry.scala
+++
@@ -1,6 +0,0 @@
-package main.scala.annotation
-
-/**
-  * Created by aaronmck on 7/20/17.
-  */
-case class CladeEntry(sample: String, clade: String, color:String)
--- TreeUtils/src/main/scala/stats/TreeSplitter.scala
+++
@@ -1,8 +0,0 @@
-package main.scala.stats
-
-/**
-  * Given a full tree, split it along sites, and recombine the results after the fact
-  */
-class TreeSplitter(statsFile: Array[Event]) {
-
-}
